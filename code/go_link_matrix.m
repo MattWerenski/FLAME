ca7703f85f9df0370eb7757a6c919c625c979ae3
MattWerenski <Mewerenski12cr@gmail.com>
2020-10-11 16:25:19 -0400

    fixed a small bug in go_link_matrix

--- code/go_link_matrix.m
+++ code/go_link_matrix.m
@@ -21,7 +21,7 @@ function [link_mat] = go_link_matrix(path, ngene, filter)
     % save the work and prune the unused weights
     i1 = i1(inuse);
     i2 = i2(inuse);
-    levels = levels(inuse);
+    weights = weights(inuse);
     
     % create the sparse matrix of weights
     %link_mat = sparse(i1,i2,levels,ngene,ngene);
